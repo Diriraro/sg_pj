69331c3a58d0b4f70379067f715ec8e835a53ca8
YounSeokHyun <skyoun00@codef.io>
2023-06-21 14:12:39 +0900

    another update

--- another.js
+++ another.js
@@ -1,3 +1,4 @@
+// Node.js의 readline, axios, axios-cookiejar-support, tough-cookie, cheerio, form-data, xlsx 모듈을 불러옵니다.
 const readline = require('readline');
 const axios = require('axios');
 const axiosCookieJarSupport = require('axios-cookiejar-support').default;
@@ -6,29 +7,33 @@ const cheerio = require('cheerio');
 const FormData = require('form-data');
 const XLSX = require('xlsx');
 
+// axios에 cookie jar 기능을 추가해줍니다.
 axiosCookieJarSupport(axios);
+// 쿠키를 저장할 수 있는 cookie jar를 생성합니다.
 const cookieJar = new tough.CookieJar();
 
+// 사용자로부터 입력을 받을 수 있는 readline 인터페이스를 생성합니다.
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-// body를 받는 함수
+// 사용자로부터 질문을 받아, 그에 대한 답을 Promise 형태로 반환하는 함수입니다.
 function promptInput(question) {
   return new Promise((resolve) => {
-      rl.question(question, (answer) => {
-          resolve(answer);
-      });
+    rl.question(question, (answer) => {
+        resolve(answer);
+    });
   });
 }
 
-async function fetchDataPOST(url, body1, body2, body3, headers) {
-  // FormData 인스턴스 생성 및 body 설정
+// 주어진 url에 POST 요청을 보내, 응답을 받아옵니다. 요청 본문은 body 객체를 form data 형식으로 변환해 전달하며,
+// 요청 헤더는 headers 객체를 사용합니다.
+async function fetchDataPOST(url, body, headers) {
   const formData = new FormData();
-  formData.append("body1", body1);
-  formData.append("body2", body2);
-  formData.append("body3", body3);
+  Object.keys(body).forEach(key => {
+    formData.append(key, body[key]);
+  });
   
   try {
     const response = await axios.post(url, formData, {
@@ -39,10 +44,11 @@ async function fetchDataPOST(url, body1, body2, body3, headers) {
     return response.data;
   } catch (error) {
     console.error("Error fetching data with POST:", error);
-    return "";
+    return null;
   }
 }
 
+// 주어진 url에 GET 요청을 보내, 응답을 받아옵니다. 요청 헤더는 headers 객체를 사용합니다.
 async function fetchDataGET(url, headers) {
   try {
     const response = await axios.get(url, {
@@ -53,61 +59,68 @@ async function fetchDataGET(url, headers) {
     return response.data;
   } catch (error) {
     console.error("Error fetching data with GET:", error);
-    return "";
+    return null;
   }
 }
 
-// HTML 파싱 및 원하는 데이터 추출
+// HTML 문자열을 파싱해 원하는 데이터를 추출하는 함수입니다. 원하는 데이터의 형태에 따라 이 함수를 수정해야 합니다.
 function parseData(html) {
   const $ = cheerio.load(html);
   const parsedData = [];
 
+  // Modify this part to match the structure of the data you are trying to extract
   // 원하는 데이터 추출(여기는 추출 코드를 추가하셔야 합니다.)
   // 예: $('div.className').each((index, element) => {
   //   parsedData.push($(element).text().trim());
   // });
 
-  return parsed;
+
+  return parsedData;
 }
 
-// 데이터를 엑셀에 저장
+// 데이터를 엑셀 파일로 저장하는 함수입니다.
 function saveToExcel(data, fileName) {
-  const wb = XLSX.utils.book_new(); // 새 워크북 생성
-  const ws = XLSX.utils.json_to_sheet(data); // JSON 데이터를 워크시트로 변환
-  XLSX.utils.book_append_sheet(wb, ws, "Sheet 1"); // 워크북에 워크시트 추가
-  XLSX.writeFile(wb, fileName); // 엑셀 파일로 저장
+  const wb = XLSX.utils.book_new();
+  const ws = XLSX.utils.json_to_sheet(data);
+  XLSX.utils.book_append_sheet(wb, ws, "Sheet 1");
+  XLSX.writeFile(wb, fileName);
 }
 
+// 메인 함수: url에 대해 GET 요청과 POST 요청을 보내고, 그 결과를 파싱하고, 엑셀 파일로 저장합니다.
 (async function main() {
-  // url1, url2, url3를 내부적으로 지정합니다.
   const url1 = "https://example1.com";
   const url2 = "https://example2.com";
   const url3 = "https://example3.com";
 
-  // 사용자 지정 헤더 정의
   const customHeaders = {
     'User-Agent': 'My-Custom-User-Agent',
     'X-Custom-Header': 'CustomHeaderValue'
   };
 
-  // Body 값을 입력 받습니다.
-  const body1 = await promptInput("Body1 값을 입력하세요: ");
-  const body2 = await promptInput("Body2 값을 입력하세요: ");
-  const body3 = await promptInput("Body3 값을 입력하세요: ");
+  const body = {
+    body1: await promptInput("Body1 값을 입력하세요: "),
+    body2: await promptInput("Body2 값을 입력하세요: "),
+    body3: await promptInput("Body3 값을 입력하세요: "),
+  };
 
   const data1 = await fetchDataGET(url1, customHeaders);
-  const parsedData1 = parseData(data1);
-  saveToExcel(parsedData1, 'output1.xlsx');
+  if(data1) {
+    const parsedData1 = parseData(data1);
+    saveToExcel(parsedData1, 'output1.xlsx');
+  }
 
-  const data2 = await fetchDataPOST(url2, body1, body2, body3, customHeaders);
-  const parsedData2 = parseData(data2);
-  saveToExcel(parsedData2, 'output2.xlsx');
+  const data2 = await fetchDataPOST(url2, body, customHeaders);
+  if(data2) {
+    const parsedData2 = parseData(data2);
+    saveToExcel(parsedData2, 'output2.xlsx');
+  }
 
   const data3 = await fetchDataGET(url3, customHeaders);
-  const parsedData3 = parseData(data3);
-  saveToExcel(parsedData3, 'output3.xlsx');
+  if(data3) {
+    const parsedData3 = parseData(data3);
+    saveToExcel(parsedData3, 'output3.xlsx');
+  }
 
-  // 사용자 키 입력을 대기하고 프로그램을 종료하는 코드를 추가합니다.
   rl.question("키를 입력하면 프로그램이 종료됩니다.", () => {
     rl.close();
   });
